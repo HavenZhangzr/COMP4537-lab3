97bb2f96cf8a6006f79a9d3f7a9399ceefdc4c6c
HavenZhangzr <158445090+HavenZhangzr@users.noreply.github.com>
2026-01-30 21:25:25 -0800

    delete Part A implement

--- app.js
+++
@@ -1,9 +0,0 @@
-const MathOperations = require('./math');
-
-const math = new MathOperations();
-const a = 10;
-const b = 5;
-const sum = math.add(a, b);
-const diff = math.subtract(a, b);
-
-console.log(`Hello Haven. The sum is ${sum}, the difference is ${diff}`);
